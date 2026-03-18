85454611e5e91663121ffdc1a4a4a635dc9adedd
levanquang123 <levanquang27122005@gmail.com>
2026-03-18 23:34:02 +0700

    chore: update build/web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -62,12 +62,12 @@ const RESOURCES = {"assets/AssetManifest.bin": "4dc04f184e899841207694d5e6fb4c0e
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "2d920a97d13653df335b4f92a4a69c34",
+"flutter_bootstrap.js": "5b5a8596221305346feb5cafa7e9345b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "ad73c222bb77f807681b6dcb82f36559",
 "/": "ad73c222bb77f807681b6dcb82f36559",
-"main.dart.js": "baf1e92c6e30940c09ab195e18cb9cb8",
+"main.dart.js": "a0fdc18c370874dce857a27b5f0035b8",
 "manifest.json": "eb0967c213ff3e1cc1170c85fc8b5515",
 "version.json": "e9eb58db72d407be27e9fa052224c304"};
 // The application shell files that are downloaded before a service worker can
