ff60ce570e1ff674f94f1cc31d0d3f383ab1096e
levanquang123 <levanquang27122005@gmail.com>
2026-03-19 11:37:22 +0700

    chore: update build/web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -62,7 +62,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4dc04f184e899841207694d5e6fb4c0e
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "84adf9a3babfabbb34bd10215ef28fb9",
+"flutter_bootstrap.js": "3c5a197083638d69edabf6d760754c74",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "ad73c222bb77f807681b6dcb82f36559",
