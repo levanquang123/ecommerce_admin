1aa1ce342724b0b4b7a5cba4fd3d232ce539c6b1
levanquang123 <levanquang27122005@gmail.com>
2026-04-27 12:12:50 +0700

    chore: refactor main_url

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -62,7 +62,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4dc04f184e899841207694d5e6fb4c0e
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "64b720885b97abebfd65c3664a72c58c",
+"flutter_bootstrap.js": "0e528bbf125067f70ad005f32405fada",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "ad73c222bb77f807681b6dcb82f36559",
