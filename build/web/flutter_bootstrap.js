f8f730de949ebf7e2ea1a6cc4daddcdfa83b254c
levanquang123 <levanquang27122005@gmail.com>
2026-04-28 10:19:37 +0700

    Update admin sidebar logo and web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4078093766"
+    serviceWorkerVersion: "2176544193"
   }
 });
