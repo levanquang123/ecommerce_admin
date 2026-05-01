0356f426dadfafc803d101fa1de16a8caedf2117
levanquang123 <levanquang27122005@gmail.com>
2026-05-01 20:56:17 +0700

    build web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -62,7 +62,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4dc04f184e899841207694d5e6fb4c0e
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "b8dff664b77e0876a6cfd47eb2840a87",
+"flutter_bootstrap.js": "19163f10f63f2ac17bece309d27c198a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "8f2f782bf792617031d74d5a91cb6879",
